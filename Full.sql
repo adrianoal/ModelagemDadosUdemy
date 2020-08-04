3f7578e1554093285022ee826700e6d2b16f4946
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-08-04 11:17:59 -0300

    62.Sub-Consultas Multiple-Row

--- Full.sql
+++ Full.sql
@@ -3086,12 +3086,82 @@ WHERE  last_name =  (SELECT last_name
    
 --------------------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------             
+62.Sub-Consultas Multiple-Row
+   
+ * Possibilitam que a Sub-Consulta retorne mais do que uma linha 
+ * Utiliza operadores de comparação multiple-row
+	
+  OPERADORES DE COMPARAÇÃO MULTIPLE-ROW:
+  --------------------------------------
+	
+  IN  --> No
+  ANY --> Qualquer 
+  ALL --> Todos 
+	
    
    
-   
-   
+--
+--
+-- Seção 12 
+-- Utilizando Sub-Consultas
+--
+-- Aula 2 - Sub-Consultas Multiple-row
+-- 
+                  
+-- Sub-Consultas Multiple-row
+
+SELECT employee_id, first_name, last_name
+FROM   employees
+WHERE  salary IN -- IN compara com uma lista de valores...
+                (SELECT    AVG(NVL(salary,0))
+                 FROM      employees
+                 GROUP BY  department_id);
+
+-- Utilizando operador NOT IN em Sub-consultas Multiple-Row
+
+SELECT employee_id, first_name, last_name
+FROM   employees
+WHERE  salary NOT IN
+                (SELECT    AVG(NVL(salary,0))
+                 FROM      employees
+                 GROUP BY  department_id);
+                 
+-- Utilizando operador ANY em Sub-consultas Multiple-Row
+
+SELECT employee_id, last_name, job_id, salary
+FROM   employees
+WHERE  salary < ANY -- (ANY) em Inglês, sig. Qualquer... 
+                   (SELECT salary
+                    FROM   employees
+                    WHERE  job_id = 'IT_PROG');
+
+-- Utilizando operador ALL em Sub-consultas Multiple-Row
+
+SELECT employee_id, last_name, job_id, salary
+FROM   employees
+WHERE  salary < ALL -- Menor do q todos
+                   (SELECT salary
+                    FROM   employees
+                    WHERE  job_id = 'IT_PROG');
+                    
+-- Cuidados com Valores Nulos em uma Sub-consulta com Operador IN
+
+SELECT emp.employee_id, emp.last_name
+FROM   employees emp
+WHERE  emp.employee_id IN (SELECT mgr.manager_id
+                           FROM employees mgr);
+
+-- Cuidados com Valores Nulos em uma Sub-consulta com Operador NOT IN
+-- Obs: Neste caso nao retornará Null
+SELECT emp.employee_id, emp.last_name
+FROM   employees emp
+WHERE  emp.employee_id NOT IN (SELECT mgr.manager_id
+                               FROM employees mgr);
+
  
-   
+--------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------             
+  
  
  
   	
