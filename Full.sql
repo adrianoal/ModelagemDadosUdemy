7531488e121aa2a71a2b00adc95bb815b1caacd7
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-08-05 08:36:45 -0300

    66.Utilizando Sub-consulta na Clausula FROM

--- Full.sql
+++ Full.sql
@@ -3252,9 +3252,30 @@ WHERE (e1.job_id, e1.salary) IN (SELECT   e2.job_id, MAX(e2.salary)
  
 --------------------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------             
- 
+66.Utilizando Sub-consultas na Cláusula FROM 
+
+--
+-- Seção 12 
+-- Utilizando Sub-Consultas
+--
+-- Aula 6 - Utilizando Sub-Consultas na Cláusula FROM
+--
+		
+-- Utilizando Sub-Consultas na Cláusula FROM
+
+SELECT empregados.employee_id, empregados.first_name, empregados.last_name, empregados.job_id, 
+       empregados.salary, 
+       ROUND(max_salary_job.max_salary,2) MAX_SALARY, 
+       empregados.salary - ROUND(max_salary_job.max_salary,2) DIFERENÇA
+FROM   employees empregados
+  LEFT JOIN (SELECT  e2.job_id, MAX(e2.salary) max_salary
+             FROM     employees e2
+             GROUP by e2.job_id) max_salary_job -- Alias da tabela --> employees e2
+       ON empregados.job_id = max_salary_job.job_id;
 
 
+--------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------             
 
  
  
