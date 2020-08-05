fb21f10d8842adfd217d0bebd9c06991ea94c37b
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-08-05 15:01:50 -0300

    70.Alterando a Estrutura da Tabela

--- Full.sql
+++ Full.sql
@@ -3944,11 +3944,80 @@ Flashback table EMPLOYEES_DEPARTMENT60 to before drop;
  
 --------------------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------             
+70.Alterando a Estrutura da Tabela 
+
+ * Podemos alterar o estado de uma tabela para o modo READ ONLY 
+ 
+ * Obs: Não permite comandos DDL ou DML 
+ 
+ Exemplo: 
+ 
+ READ ONLY  --> SOMENTE LEITURA
+ READ WRITE --> PERMITE LEITURA E ESCRITA(DDL e DML)
+ 
+ 
+-- ALTER TABLE – READ ONLY
+
+ALTER TABLE employees READ ONLY;
+
+-- ALTER TABLE – READ WRITE
+
+ALTER TABLE employees READ WRITE;
 
  
  
+--
+-- Seção 15 
+-- Comandos DDL 
+--
+-- Aula 2 - Alterando a Estrutura da Tabela
+--
+
+-- ALTER TABLE – Adicionando uma coluna
+
+ALTER TABLE projects 
+ADD (department_id NUMBER(3)); 
+
+DESC projects
+
+-- ALTER TABLE – Removendo uma coluna
+
+ALTER TABLE projects 
+DROP COLUMN department_id; 
+
+DESC projects
+
+-- ALTER TABLE – Adicionando uma coluna
+
+ALTER TABLE projects
+ADD (department_id NUMBER(4) NOT NULL); 
+
+DESC projects
+
+-- ALTER TABLE – Adicionando uma coluna
+
+ALTER TABLE projects
+MODIFY (project_code VARCHAR2(6));
+                             
+DESC PROJECTS
+               
+-- ALTER TABLE – Renomeando uma coluna
+
+ALTER TABLE projects
+RENAME COLUMN project_name TO PROJECT_TESTE;
+
+DESC projects
+
+-- ALTER TABLE – READ ONLY
+
+ALTER TABLE employees READ ONLY;
+
+-- ALTER TABLE – READ WRITE
+
+ALTER TABLE employees READ WRITE; 
  
- 
+--------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------              
 
   
  
