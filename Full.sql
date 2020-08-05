2a7836357dab61ee48aa24521dc7c43fed0ced5d
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-08-05 08:21:19 -0300

    64.Utilizando Sub-Consulta correlacionada

--- Full.sql
+++ Full.sql
@@ -3202,7 +3202,34 @@ WHERE  NOT EXISTS (SELECT e.department_id
  
 --------------------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------             
-64.
+64.Utilizando Sub-consulta correlacionada
+
+
+-- Seção 12 
+-- Utilizando Sub-Consultas
+--
+-- Aula 4 - Utilizando Sub-Consulta Correlacionada
+--
+  
+-- Utilizando Sub-Consultas Correlacionadas
+               
+SELECT e1.employee_id, e1.first_name, e1.last_name, e1.department_id, e1.salary
+FROM   employees e1
+WHERE  e1.salary >= (SELECT    TRUNC(AVG(NVL(salary,0)),2)
+                     FROM      employees e2
+                     WHERE     e1.department_id = e2.department_id);
+-- Testando a media salario por departamento: 
+SELECT    TRUNC(AVG(NVL(salary,0)),0)
+FROM      employees e2
+WHERE     e2.department_id = 90;
+
+SELECT    TRUNC(AVG(NVL(salary,0)),0)
+FROM      employees e2
+WHERE     e2.department_id = 60;
+
+
+--------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------             
   	
  
  
